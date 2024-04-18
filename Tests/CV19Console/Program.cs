90187e66f4cff50f438f7b5952060df1d958ea2d
brekembe <andrew083197@gmail.com>
2024-04-18 20:43:02 +0300

    Извлечение данных по датам записей

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,18 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace CV19Console
 {
     class Program
     {
         const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+        
+        private static async Task<Stream> GetDataStream()
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
+            return await response.Content.ReadAsStreamAsync();
+        }
+
+        private static IEnumerable<string> GetDataLines()
+        {
+            using var data_stream = GetDataStream().Result;
+            using var data_reader = new StreamReader(data_stream);
+
+            while (!data_reader.EndOfStream)
+            {
+                var line = data_reader.ReadLine();
+                if (string.IsNullOrWhiteSpace(line))
+                    continue;
+
+                yield return line;
+            }
+        }
+
+        private static DateTime[] GetDates() => GetDataLines()
+            .First()
+            .Split(',')
+            .Skip(4)
+            .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
+            .ToArray();
+
+
+        
         static void Main(string[] args)
         {
             //WebClient client = new WebClient
-            HttpClient client = new HttpClient();
+            /*HttpClient client = new HttpClient();
             var response = client.GetAsync(data_url).Result;
             string csv_str = response.Content.ReadAsStringAsync().Result;
-            Console.WriteLine(csv_str.ToString());
+
+            Console.WriteLine(csv_str.ToString());*/
+
+            /*foreach (var data_line in GetDataLines())
+                Console.WriteLine(data_line);*/
+
+            var dates = GetDates();
+            Console.WriteLine(string.Join("\r\n", dates));
+
             Console.ReadLine();
             
         }
