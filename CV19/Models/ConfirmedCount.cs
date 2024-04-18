bc55e23dfdb35829cdde3b9cbaaa7692b9f35255
brekembe <andrew083197@gmail.com>
2024-04-18 21:29:55 +0300

    Добавление модели

---
+++ CV19/Models/ConfirmedCount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19.Models
+{
+    internal struct ConfirmedCount
+    {
+        public DateTime Date { get; set; }
+        public int Count { get; set; }
+    }
+}
