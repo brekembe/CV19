57bbd5c5d7d461f90b54ff0471570b3c20039e3d
brekembe <andrew083197@gmail.com>
2024-04-18 20:20:46 +0300

    Запрос к удаленному серверу для получения сырых данных

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace CV19Console
 {
     class Program
     {
+        const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //WebClient client = new WebClient
+            HttpClient client = new HttpClient();
+            var response = client.GetAsync(data_url).Result;
+            string csv_str = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(csv_str.ToString());
+            Console.ReadLine();
+            
         }
     }
 }
